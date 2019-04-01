759ad8d794158b5546c3e6fc7cd10dce615c31c7
nnikka <nikakhurashvili@gmail.com>
2019-04-01 18:29:07 +0400

    create player component

---
+++ src/components/player/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ss-player-container">
+    <img src="@/assets/Player/Robot_idle-1_01.png" />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.ss-player-container {
+  position: absolute;
+  z-index: 110;
+}
+</style>
--- src/views/game/index.vue
+++ src/views/game/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div style="width: 800px; height: 400px; position: relative; display: inline-block;">
     <game-world />
+    <player />
   </div>
 </template>
 
 <script>
 import GameWorld from '@/components/gameWorld'
+import Player from '@/components/player'
 
 export default {
   name: 'game',
   components: {
-    GameWorld
+    GameWorld,
+    Player
   }
 }
 </script>
