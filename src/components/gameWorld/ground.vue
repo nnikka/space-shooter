684dce805168f6772f6ce892d271842d37ac762a
nnikka <nikakhurashvili@gmail.com>
2019-04-01 15:00:41 +0400

    create game world ground component

---
+++ src/components/gameWorld/ground.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="ss-game-world-ground-container">
+    <img class="ss-bush" src="@/assets/Background/bush.png" />
+    <img class="ss-grass" src="@/assets/Background/grass2.png" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.ss-game-world-ground-container {
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+}
+.ss-game-world-ground-container .ss-grass {
+  width: 100%;
+  height: auto;
+}
+.ss-game-world-ground-container .ss-bush {
+  width: 100%;
+  height: auto;
+  margin-bottom: -4.5%;
+}
+</style>
--- src/components/gameWorld/index.vue
+++ src/components/gameWorld/index.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-    World
+  <div class="ss-game-world-container">
+    <ground />
   </div>
 </template>
 
 <script>
-export default {
+import Ground from './ground'
 
+export default {
+  name: 'GameWold',
+  components: {
+    Ground
+  }
 }
 </script>
 
 <style>
-
+  .ss-game-world-container {
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-image: url("~@/assets/Background/sky1.png");
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    background-position: center top;
+    background-attachment: fixed;
+    overflow: hidden;
+  }
 </style>
