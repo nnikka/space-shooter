df33b8aa5c6b390b14ac91c9c8ddfcd90306086e
nnikka <nikakhurashvili@gmail.com>
2019-04-01 14:33:04 +0400

    create game world component

---
+++ src/views/game/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <game-world />
+  </div>
+</template>
+
+<script>
+import GameWorld from '@/components/gameWorld'
+
+export default {
+  name: 'game',
+  components: {
+    GameWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
