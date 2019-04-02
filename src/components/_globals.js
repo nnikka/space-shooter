0ee9aa1c6cf4b77f2d5a69ef113d596ee2b36a74
nnikka <nikakhurashvili@gmail.com>
2019-04-02 10:10:22 +0400

    create enemy component

--- src/components/_globals.js
+++ src/components/_globals.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Animate from './animate'
+import Enemy from './enemy'
 
 const requireComponents = {
-  'ss-animate': Animate
+  'ss-animate': Animate,
+  'ss-enemy': Enemy
 }
 
 Object.keys(requireComponents).forEach(name => {
