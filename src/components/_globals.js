c07eea31c78509dcfe70571b7995d4fdac39f19e
nnikka <nikakhurashvili@gmail.com>
2019-04-02 10:14:47 +0400

    create bullet component

--- src/components/_globals.js
+++ src/components/_globals.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Animate from './animate'
 import Enemy from './enemy'
+import Bullet from './bullet'
 
 const requireComponents = {
   'ss-animate': Animate,
-  'ss-enemy': Enemy
+  'ss-enemy': Enemy,
+  'ss-Bullet': Bullet
 }
 
 Object.keys(requireComponents).forEach(name => {
