39d1d3f4b009d606626c15343e2f27270a025393
nnikka <nikakhurashvili@gmail.com>
2019-04-09 15:19:34 +0400

    set up vuetify

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import App from './App'
 import router from './router'
 import store from './store'
 import globalMixin from './mixins/global'
-
+import Vuetify from 'vuetify'
 
 import '@/components/_globals'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 
 Vue.mixin(globalMixin)
+
+Vue.use(Vuetify)
 
 new Vue({
   el: '#app',
